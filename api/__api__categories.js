caa2c94647eb8cd009453de01781f86b05566eb1
euidong <48043626+euidong@users.noreply.github.com>
2022-04-21 09:29:16 +0000

    Deploying to gh-pages from  @ 8c2d8daacaf15d2c333e521db624bac5a5b17b67 🚀

--- api/__api__categories.js
+++
@@ -1,4 +0,0 @@
-const { adaptor } = require('next-export-api')
-const { default: handler } = require('../../.next/server/pages/api/categories.js')
-
-module.exports.handler = adaptor(handler)
--- api/__api__tags.js
+++
@@ -1,4 +0,0 @@
-const { adaptor } = require('next-export-api')
-const { default: handler } = require('../../.next/server/pages/api/tags.js')
-
-module.exports.handler = adaptor(handler)
