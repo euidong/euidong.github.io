99f0c29a9e7371232e485f28a40d4eec9a3303ea
justicedong <justicedong@naver.com>
2022-04-23 10:00:00 +0900

    Fix: Title overflow in category

--- components/Card/Row/List.tsx
+++ components/Card/Row/List.tsx
@@ -1,6 +1,7 @@
 import styles from "./RowCard.module.scss";
 import RowCard from ".";
 import { PostMetadata } from "../../../types/posts";
+import { formatTitle } from "../../../lib/utils";
 
 interface Props {
   subject: string;
@@ -10,7 +11,7 @@ interface Props {
 const RowCardList = ({ subject, posts }: Props) => {
   return (
     <div className={styles.row_card__list__background}>
-      <h1 className={styles.row_card__list__title}>{subject}</h1>
+      <h1 className={styles.row_card__list__title}>{formatTitle(subject)}</h1>
       <ul className={styles.row_card__list__wrapper}>
         {posts.map((post: any, idx: number) => (
           <RowCard
