b3139e5df751535982fb05a64fa2817ed812b27f
justicedong@naver.com <a@as-MacBook-Pro.local>
2023-07-12 10:05:10 +0900

    Update: Google Ads Style

--- components/GoogleAds/Multiplex.tsx
+++ components/GoogleAds/Multiplex.tsx
@@ -4,7 +4,7 @@ const MultiplexGoogleAds = () => {
   return (
     <ins
       className="adsbygoogle"
-      style={{ display: "block" }}
+      style={{ display: "block", textAlign: "center" }}
       data-ad-format="autorelaxed"
       data-ad-client="ca-pub-7452732177557701"
       data-ad-slot="3654773972"
